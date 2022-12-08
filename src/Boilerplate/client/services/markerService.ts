b5e9c5c400095b09a0d2528a1c33b58da9b75b9d
cRiZZlyy <cosmicesl@hotmail.com>
2022-12-09 00:37:21 +0100

    better code & improve markers

--- src/Boilerplate/client/services/markerService.ts
+++ src/Boilerplate/client/services/markerService.ts
@@ -31,8 +31,26 @@ export function drawMarker(type: number, pos: alt.IVector3, dir: alt.IVector3, r
     );
 }
 // Create Markers on the Positions
-alt.everyTick(() => {
-    drawMarker(30, new alt.Vector3(1369.8944091796875, -593.9959106445312, 74.40187072753906), new alt.Vector3(0, 0, 0), new alt.Vector3(0, 0, 0), new alt.Vector3(1, 1, 1), 50, 205, 50, 100);
-    drawMarker(20, new alt.Vector3(1364.36669921875, -604.4774169921875, 74.487548828125), new alt.Vector3(0, 0, 0), new alt.Vector3(0, 0, 0), new alt.Vector3(1, 1, 1), 50, 205, 50, 100);
-    drawMarker(36, new alt.Vector3(1356.50634765625, -585.0873413085938, 74.49462890625), new alt.Vector3(0, 0, 0), new alt.Vector3(0, 0, 0), new alt.Vector3(1, 1, 1), 50, 205, 50, 100);
-});
+function createMarkers() {
+    // EQUIP MARKER
+    const markers = new alt.Utils.Marker(new alt.Vector3(1369.8944091796875, -593.9959106445312, 74.40187072753906),
+        {
+            type: 30, color: new alt.RGBA(50, 205, 50, 100),
+            scale: new alt.Vector3(1, 1, 1)
+        })
+    // CLOTHE MARKER
+    new alt.Utils.Marker(new alt.Vector3(1364.36669921875, -604.4774169921875, 74.487548828125),
+        {
+            type: 20, color: new alt.RGBA(50, 205, 50, 100),
+            scale: new alt.Vector3(1, 1, 1)
+        })
+    // VEHICLE SPAWN MARKER
+    new alt.Utils.Marker(new alt.Vector3(1356.50634765625, -585.0873413085938, 74.49462890625),
+        {
+            type: 36, color: new alt.RGBA(50, 205, 50, 100),
+            scale: new alt.Vector3(1, 1, 1)
+        })
+}
+createMarkers()
+/*     drawMarker(20, new alt.Vector3(1364.36669921875, -604.4774169921875, 74.487548828125), new alt.Vector3(0, 0, 0), new alt.Vector3(0, 0, 0), new alt.Vector3(1, 1, 1), 50, 205, 50, 100);
+    drawMarker(36, new alt.Vector3(1356.50634765625, -585.0873413085938, 74.49462890625), new alt.Vector3(0, 0, 0), new alt.Vector3(0, 0, 0), new alt.Vector3(1, 1, 1), 50, 205, 50, 100); */
