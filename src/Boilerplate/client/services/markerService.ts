2d01851b261d90661bd6e19896eea3e6ac0a7b9e
cRiZZlyy <cosmicesl@hotmail.com>
2022-12-09 01:30:49 +0100

    remov old markerService

--- src/Boilerplate/client/services/markerService.ts
+++ src/Boilerplate/client/services/markerService.ts
@@ -1,35 +1,5 @@
 import * as alt from 'alt-client';
-import * as native from 'natives'
 
-// DRAW MARKER
-export function drawMarker(type: number, pos: alt.IVector3, dir: alt.IVector3, rot: alt.IVector3, scale, r, g, b, alpha) {
-    native.drawMarker(
-        type,
-        pos.x,
-        pos.y,
-        pos.z,
-        dir.x,
-        dir.y,
-        dir.z,
-        rot.x,
-        rot.y,
-        rot.z,
-        scale.x,
-        scale.y,
-        scale.z,
-        r,
-        g,
-        b,
-        alpha,
-        false,
-        true,
-        2,
-        false,
-        undefined,
-        undefined,
-        false
-    );
-}
 // Create Markers on the Positions
 function createMarkers() {
     // EQUIP MARKER
